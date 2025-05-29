d12ec358143162c34ce994ccf18b2209b58a3942
IsequielNascimento <isequielnascimento@gmail.com>
2025-05-29 11:03:34 -0300

    feat: search logic

--- main.c
+++ main.c
@@ -32,5 +32,33 @@ void getZarr(const char *str, int Z[], int n) {
 
 
 
+// Searches for the pattern in the text using Z algorithm
+void search(const char *text, const char *pattern) {
+    char concat[MAX_PATTERN_LEN + 1 + MAX_TEXT_LEN + 1]; // pattern + '$' + text + '\0'
+    int Z[MAX_PATTERN_LEN + 1 + MAX_TEXT_LEN];           // Z array size matches concat length
 
+    // Form the concatenated string
+    int pat_len = strlen(pattern);
+    int text_len = strlen(text);
+    int concat_len = pat_len + 1 + text_len;
+
+    // Constructing the concat string
+    int i, j = 0;
+    for (i = 0; i < pat_len; ++i)
+        concat[j++] = pattern[i];
+    concat[j++] = '$';
+    for (i = 0; i < text_len; ++i)
+        concat[j++] = text[i];
+    concat[j] = '\0';
+
+    // Get Z array for concat
+    getZarr(concat, Z, concat_len);
+
+    // Loop through Z array to find pattern matches
+    for (i = 0; i < concat_len; ++i) {
+        if (Z[i] == pat_len) {
+            printf("Pattern found at index %d\n", i - pat_len - 1);
+        }
+    }
+}
 
