5c54570cd2176d7ce395f0745d6c0f0b439d7ea6
IsequielNascimento <isequielnascimento@gmail.com>
2025-08-05 23:16:37 -0300

    refact-Otimização do código

--- T3/timer.c
+++ T3/timer.c
@@ -1,49 +1,45 @@
 #include <stdio.h>
 #include <time.h>
-#include <string.h>
 
 #define MAX_TEXT_SIZE 1100
 #define MAX_PATTERN_SIZE 4
 #define MAX_OCCURRENCES 20
 #define N_EXECUCOES 1000
 
-
+static char concat_buffer[MAX_PATTERN_SIZE + 1 + MAX_TEXT_SIZE];
+static int z_array_buffer[MAX_PATTERN_SIZE + 1 + MAX_TEXT_SIZE];
 
 void compute_z_array(const char *s, int z[], int length) {
     int l = 0, r = 0;
     z[0] = 0;
     for (int i = 1; i < length; i++) {
-        if (i <= r)
-            z[i] = (r - i + 1 < z[i - l]) ? (r - i + 1) : z[i - l];
-        else
-            z[i] = 0;
+        int zi = 0;
 
-        while (i + z[i] < length && s[z[i]] == s[i + z[i]])
-            z[i]++;
+        if (i <= r) {
+            int k = z[i - l];
+            if (r - i + 1 < k) k = r - i + 1;
+            zi = k;
+        }
 
-        if (i + z[i] - 1 > r) {
+        while (i + zi < length && s[zi] == s[i + zi]) {
+            zi++;
+        }
+
+        z[i] = zi;
+
+        if (i + zi - 1 > r) {
             l = i;
-            r = i + z[i] - 1;
+            r = i + zi - 1;
         }
     }
 }
 
-__uint8_t z_search(const char *text, const char *pattern, int text_len, int pattern_len, char occurrences[]) {
-     char concat_buffer[MAX_PATTERN_SIZE + 1 + MAX_TEXT_SIZE];
-     int z_array_buffer[MAX_PATTERN_SIZE + 1 + MAX_TEXT_SIZE];
-    int concat_len = pattern_len + 1 + text_len;
-
-     strncpy(concat_buffer, pattern, pattern_len);
-    concat_buffer[pattern_len] = '$'; // Adiciona o separador
-
-    // Copia o texto para o buffer de concatenação
-    strncpy(concat_buffer + pattern_len + 1, text, text_len);
-
+__uint8_t z_search(int concat_len, int pattern_len, char occurrences[]) {
     compute_z_array(concat_buffer, z_array_buffer, concat_len);
 
     char count = 0;
-    for (int i = pattern_len + 1; i < concat_len; i++) {
-        if (z_array_buffer[i] == pattern_len && count < MAX_OCCURRENCES) {
+    for (int i = pattern_len + 1; i < concat_len && count < MAX_OCCURRENCES; i++) {
+        if (z_array_buffer[i] == pattern_len) {
             occurrences[count++] = i - pattern_len - 1;
         }
     }
@@ -55,10 +51,20 @@ int main(void) {
     static const char pattern[MAX_PATTERN_SIZE] = {'A','T','G','C'};
     char occurrences[MAX_OCCURRENCES];
 
+    for (int i = 0; i < MAX_PATTERN_SIZE; i++) {
+        concat_buffer[i] = pattern[i];
+    }
+    concat_buffer[MAX_PATTERN_SIZE] = '$';
+    for (int i = 0; i < MAX_TEXT_SIZE; i++) {
+        concat_buffer[MAX_PATTERN_SIZE + 1 + i] = fixed_text[i];
+    }
+
+    int concat_len = MAX_PATTERN_SIZE + 1 + MAX_TEXT_SIZE;
+
     clock_t start = clock();
 
     for (int i = 0; i < N_EXECUCOES; i++) {
-        z_search(fixed_text, pattern, MAX_TEXT_SIZE, MAX_PATTERN_SIZE, occurrences);
+        z_search(concat_len, MAX_PATTERN_SIZE, occurrences);
     }
 
     clock_t end = clock();
@@ -70,3 +76,4 @@ int main(void) {
 
     return 0;
 }
+
